0262afc0c8f548326a237ca7819a63b9978cc3a3
ledooooo <wal33dl33do@gmail.com>
2025-12-27 13:32:34 +0200

    Update AdminDashboard.tsx

--- pages/AdminDashboard.tsx
+++ pages/AdminDashboard.tsx
@@ -853,6 +853,7 @@ const handleImport = async (data: any[]) => {
     alert(`تقرير استيراد البصمات النهائي:\n--------------------------\n- سجلات جديدة تم رفعها: ${inserted}\n- سجلات حالية تم تحديثها (لوجود تغيير): ${updated}\n- سجلات تم إهمالها (مطابقة تماماً): ${skipped}`);
     onRefresh(); 
   };
+
   
   return (
     <div className="space-y-6">
