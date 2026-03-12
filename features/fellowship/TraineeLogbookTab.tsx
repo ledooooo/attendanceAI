335df6a9358d48cee28e4251b6dc31fa022f5b40
ledooooo <wal33dl33do@gmail.com>
2026-03-12 03:02:12 +0200

    Create TraineeLogbookTab.tsx

---
+++ features/fellowship/TraineeLogbookTab.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function TraineeLogbookTab({ employeeId }: { employeeId: string }) {
+  return <div className="p-6 text-center text-gray-500 font-bold mt-20">جاري تجهيز سجل الحالات (Logbook)...</div>;
+}
