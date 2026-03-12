59fa63a2dadee838e7c04f3d8a514c9ec43ec204
ledooooo <wal33dl33do@gmail.com>
2026-03-12 13:10:13 +0200

    Update TraineeDashboard.tsx

--- features/fellowship/TraineeDashboard.tsx
+++ features/fellowship/TraineeDashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../../../../supabaseClient';
+import { supabase } from '../../supabaseClient';
 import toast from 'react-hot-toast';
 import { 
   GraduationCap, Users, Stethoscope, Link as LinkIcon, 
