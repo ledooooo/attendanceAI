de4cce08bbc82f4b7bcaa7ea148f6ceff1910d1e
ledooooo <wal33dl33do@gmail.com>
2026-03-12 03:01:52 +0200

    Create tabs/TraineeOverviewTab.tsx

---
+++ features/fellowship/tabs/TraineeOverviewTab.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function TraineeOverviewTab({ employeeId }: { employeeId: string }) {
+  return <div className="p-6 text-center text-gray-500 font-bold mt-20">جاري تحميل بيانات التقدم الخاصة بك...</div>;
+}
