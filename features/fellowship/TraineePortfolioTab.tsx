c5d7d83f9cd5be49f48981862e0601e3ba983c2b
ledooooo <wal33dl33do@gmail.com>
2026-03-12 03:23:17 +0200

    Delete TraineePortfolioTab.tsx

--- features/fellowship/TraineePortfolioTab.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default function TraineePortfolioTab({ employeeId }: { employeeId: string }) {
-  return <div className="p-6 text-center text-gray-500 font-bold mt-20">جاري تجهيز ملف الإنجاز...</div>;
-}
