cb1ba96692c766286f49206184acf5a879459e34
ledooooo <wal33dl33do@gmail.com>
2026-03-25 15:32:07 +0200

    Update AdminQueueSettings.tsx

--- queue/AdminQueueSettings.tsx
+++ queue/AdminQueueSettings.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { supabase } from '../../supabaseClient';
+import { supabase } from '../supabaseClient';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 import { 
